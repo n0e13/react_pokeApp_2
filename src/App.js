84e613141a42479f8c7d27040f221c79557e30e3
n0e <noemy.garcia@gmail.com>
2022-05-04 16:32:22 +0200

    [ADD] Axios

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import { BrowserRouter } from 'react-router-dom';
 import Header from './containers/Header';
 import Main from './containers/Main';
 import Footer from './containers/Footer';
+import { pokemonContext } from './context/pokemonContext';
+import { useState } from 'react';
 
 function App() {
+
+  const [pokeList, setPokeList] = useState([]);
+
+  const savePokeList = (newPokeList) => setPokeList(newPokeList);
+
+  const pokeData = {
+    pokeList,
+    savePokeList
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        <Main />
+        <pokemonContext.Provider value={pokeData}>
+          <Main />
+        </pokemonContext.Provider>
         <Footer />
       </BrowserRouter>
     </div>
--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import { POKEMON_URI } from '../../constants/index';
+import { pokemonContext } from '../../context/pokemonContext';
+import CustomButtom from '../CustomButton';
 
 const PokemonList = () => {
-  return <div>PokemonList</div>;
+
+  const { pokeList, savePokeList } = useContext(pokemonContext);
+
+  useEffect(
+    () => {
+      const fetchData = async () => {
+        try {
+          if (input !== '') {
+            const resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${input}`);
+            const data = await resp.data;
+            // setPokemons({ data });
+            setPokemons([data, ...pokemons]);
+            setInput('');
+          }
+        } catch (error) {
+          console.log(error);
+          setInput(''); // No pinta nada
+        }
+      };
+      fetchData();
+    },
+    [input] // eslint-disable-line react-hooks/exhaustive-deps
+  );
+
+  return (
+    <div>
+
+    </div>
+  );
 };
 
 export default PokemonList;
