465c79ab798152b0b6eadf2fe7235b5489d2490c
n0e <noemy.garcia@gmail.com>
2022-05-06 23:26:26 +0200

    [ADD] Close detail

--- src/components/CustomCloseButton/CustomCloseButton.jsx
+++ src/components/CustomCloseButton/CustomCloseButton.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const CustomCloseButton = () => {
-  return <div>CustomCloseButton</div>;
+  const navigate = useNavigate();
+const handleClick = () => {
+navigate('/');
+}
+
+  return <button onClick={handleClick}>X</button>;
 };
 
 export default CustomCloseButton;
--- src/components/PokemonDetail/PokemonDetail.jsx
+++ src/components/PokemonDetail/PokemonDetail.jsx
@@ -4,11 +4,15 @@ import queryString from 'query-string';
 import CustomCloseButton from '../CustomCloseButton';
 
 const PokemonDetail = () => {
+
   const location = useLocation();
   const { name, img, typeOne, typeTwo } = queryString.parse(location.search);
 
 
-  return <div> Nombre: {name} <img src={img} /> Tipos: {typeOne} {typeTwo !== '' ? `y ${typeTwo}` : ''}</div>;
+  return (
+    
+  <div> <CustomCloseButton/><p>Nombre: {name} <img src={img} /> Tipos: {typeOne} {typeTwo !== '' ? `y ${typeTwo}` : ''}</p></div>
+  );
 };
 
 export default PokemonDetail;
