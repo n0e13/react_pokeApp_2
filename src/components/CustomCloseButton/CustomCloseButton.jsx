8f9311eb71b3f94eba79d844503e568acf02ceeb
n0e <noemy.garcia@gmail.com>
2022-05-06 23:19:18 +0200

    [ADD] Detail logic

---
+++ src/components/CustomCloseButton/CustomCloseButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomCloseButton = () => {
+  return <div>CustomCloseButton</div>;
+};
+
+export default CustomCloseButton;
--- src/components/PokemonDetail/PokemonDetail.jsx
+++ src/components/PokemonDetail/PokemonDetail.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import queryString from 'query-string';
+import CustomCloseButton from '../CustomCloseButton';
 
 const PokemonDetail = () => {
-  return <div>PokemonDetail</div>;
+  const location = useLocation();
+  const { name, img, typeOne, typeTwo } = queryString.parse(location.search);
+
+
+  return <div> Nombre: {name} <img src={img} /> Tipos: {typeOne} {typeTwo !== '' ? `y ${typeTwo}` : ''}</div>;
 };
 
 export default PokemonDetail;
